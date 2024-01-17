6c52891042eaafd30de90cc57534fd131a93487d
Matt Christopherson <matthew.christopherson27@gmail.com>
2024-01-17 14:48:55 -0600

    Got most of the HTML and CSS framework set up.

---
+++ script.js
@@ -0,0 +1,8 @@
+var startBtn = document.querySelector("#start");
+var timeLog = document.querySelector("#timer");
+
+function letsBegin() {
+    
+}
+
+startBtn.addEventListener("click", letsBegin); 
